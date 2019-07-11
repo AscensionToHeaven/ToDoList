cbcf1608e4f529cf9e15628ac27fefb631a686a9
AscensionToHeaven <52785883+AscensionToHeaven@users.noreply.github.com>
2019-07-11 23:05:00 +0800

    init with umi

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+
+export const dva = {
+  config: {
+    onError(err: ErrorEvent) {
+      err.preventDefault();
+      // tslint:disable-next-line
+      console.error(err.message);
+    },
+  },
+};
